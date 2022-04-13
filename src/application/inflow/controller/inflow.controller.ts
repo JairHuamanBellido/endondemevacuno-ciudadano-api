50616197452df29dd7583b349d667ac1bb1e7815
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2022-04-13 05:45:09 -0500

    Add service for get flow by hour

--- src/application/inflow/controller/inflow.controller.ts
+++ src/application/inflow/controller/inflow.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { InflowDIToken } from 'src/domain/inflow/di/InflowDIToken';
 import { Inflow } from 'src/domain/inflow/model/Inflow';
 import { GetInflowBetweenDatesService } from 'src/domain/inflow/service/GetInflowBetweenDatesService';
+import { Period } from 'src/domain/inflow/types/Period';
 
 @Controller('inflow')
 export class InflowController {
@@ -15,11 +16,13 @@ export class InflowController {
     @Query('startDate') startDate: string,
     @Query('endDate') endDate: string,
     @Query('vaccineCenterId') vaccineCenterId: string,
+    @Query('period') period: Period,
   ): Promise<Inflow[]> {
     return await this.getInflowBetweenDatesService.execute(
       startDate,
       endDate,
       vaccineCenterId,
+      period,
     );
   }
 }
--- src/domain/inflow/service/GetInflowBetweenDatesService.ts
+++ src/domain/inflow/service/GetInflowBetweenDatesService.ts
@@ -1,5 +1,6 @@
 import { IInflowRepository } from '../interface/IInflowRepository';
 import { Inflow } from '../model/Inflow';
+import { Period } from '../types/Period';
 
 export class GetInflowBetweenDatesService {
   constructor(private readonly inflowRepository: IInflowRepository) {}
@@ -8,11 +9,37 @@ export class GetInflowBetweenDatesService {
     startDate: string,
     endDate: string,
     vaccineCenterId: string,
-  ): Promise<Inflow[]> {
-    return await this.inflowRepository.getBetweenDates(
+    period: Period,
+  ): Promise<any> {
+    const data = await this.inflowRepository.getBetweenDates(
       startDate,
       endDate,
       vaccineCenterId,
     );
+
+    if (period === 'hour') {
+      const hour = new Date(startDate).getHours();
+      return this.getInflowByHour(data, hour.toString());
+    }
+    return [];
+  }
+
+  private getInflowByHour(inflow: Inflow[], hour: string) {
+    // It generates an array from 0 - 11
+    const arr = Array.from({ length: 12 }, (_, index) => index);
+
+    const addZeroToLeft = (value: string) => value.padStart(2, '0');
+
+    // It generates an array with objects
+    // {`hour:minute`: value} -> [{'20:15': 21} {'20:20': 12}]
+    const generateTime = (_hour: string, index: number) => {
+      const hour = addZeroToLeft(_hour);
+      const minute = addZeroToLeft((index * 5).toString());
+      return `${hour}:${minute}`;
+    };
+
+    return arr.map((v: number) => ({
+      [`${generateTime(hour, v)}`]: inflow[v] ? inflow[v].peopleEntering : 0,
+    }));
   }
 }
